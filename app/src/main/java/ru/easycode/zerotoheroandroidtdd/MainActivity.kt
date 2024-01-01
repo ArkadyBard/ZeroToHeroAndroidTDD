fb9468f7d6b9cdfbb933e7e6407d0b9c400978e8
Arkady <asdarka1994@gmail.com>
2024-01-01 13:59:49 +0800

    Test_006 passed by code (without xml)

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -3,6 +3,7 @@ package ru.easycode.zerotoheroandroidtdd
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -10,11 +11,25 @@ class MainActivity : AppCompatActivity() {
     private lateinit var textView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        textView = findViewById(R.id.titleTextView)
-        button = findViewById(R.id.changeButton)
+        val linearLayout = LinearLayout(this)
+        linearLayout.orientation = LinearLayout.VERTICAL
+        linearLayout.id = R.id.rootLayout
+
+        textView = TextView(this)
+        textView.id = R.id.titleTextView
+        textView.text = "Hello World!"
+        linearLayout.addView(textView)
+
+        button = Button(this)
+        button.id = R.id.changeButton
+        button.text = "change"
+        linearLayout.addView(button)
+
+        setContentView(linearLayout)
+
         button.setOnClickListener {
             textView.text = "I am an Android Developer!"
         }
+
     }
 }
